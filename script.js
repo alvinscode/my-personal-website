b571cf05647af547d704c5e94b172bb07ef74c8b
Alvin Li <aflip24@gmail.com>
2023-02-23 08:41:59 -0600

    initial changes to index.html, script.js, and style.css

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-
+h2.textContent = "My name is Alvin Li. I am a Software Engineering student from Chicago, Illinois.";
+h2.addEventListener("mouseover", function() {
+    h2.style.fontSize = '50px';
+    h2.style.fontWeight = 'bold';
+    h2.style.fontKerning = 'none';
+});
+h2.addEventListener("mouseout", function() {
+    h2.style.fontSize = '25px';
+    h2.style.fontWeight = 'lighter';
+    h2.style.fontKerning = 'normal';
+});
 document.querySelector("body").appendChild(h2);
