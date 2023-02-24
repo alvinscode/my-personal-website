3c17ed1ab665dcc3f2e73f43bcf10c82e5339a02
Alvin Li <aflip24@gmail.com>
2023-02-23 22:47:06 -0600

    seperated description text, added more animation to new description text

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-//mouseover text effect
+//mouseover text effect, inserting h2 into page
 const h2 = document.createElement("h2");
-h2.textContent = "My name is Alvin Li. I am a Software Engineering student from Chicago, Illinois.";
+h2.textContent = "My name is Alvin Li.";
 h2.addEventListener("mouseover", function() {
     h2.style.fontSize = '50px';
     h2.style.fontWeight = 'bold';
     h2.style.fontKerning = 'none';
 });
 h2.addEventListener("mouseout", function() {
-    h2.style.fontSize = '25px';
+    h2.style.fontSize = '149%';
 });
 document.querySelector("body").appendChild(h2);
 //end mouseover text effect
